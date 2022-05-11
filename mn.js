fd43886c692e353ca39b66b825bd71bb42832b49
ebrahama <93477372+ebrahama@users.noreply.github.com>
2022-05-11 13:50:14 +0200

    Add files via upload

--- mn.js
+++ mn.js
@@ -20,5 +20,44 @@
     }
 
 
-    // img // tex sec
+    // img 
+
+    const min = document.querySelector('.pro1')
+    
+    function ca1(){
+        min.src ='./images/image-product-1.jpg' ;
+    }
+    function ca2(){
+        min.src ='./images/image-product-2.jpg' ;
+    }
+    function ca3(){
+        min.src ='./images/image-product-3.jpg' ;
+    }
+    function ca4(){
+        min.src ='./images/image-product-4.jpg' ;
+    }
+
+// tex atc
+
+const add = document.getElementById('add');
+const nag = document.getElementById('nag');
+let nam = document.getElementById('nam');
+let nom = 0
+
+add.addEventListener('click' , function(){
+
+    re = nom++
+    nam.innerText = re
+});
+
+nag.addEventListener('click' , function(){
+
+    re = nom--
+    nam.innerText = re
+
+
+});
+
+//cart
+
 

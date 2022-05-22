47b53b3ee5d6b7a740aa2475d890ef7ab1b7c066
ebrahama <93477372+ebrahama@users.noreply.github.com>
2022-05-22 19:00:43 +0200

    Add files via upload

--- mn.js
+++ mn.js
@@ -1,8 +1,21 @@
+
+const add = document.getElementById('add');
+const nag = document.getElementById('nag');
+let nam = document.getElementById('nam');
+
+let nom = 0;
+
+const pri = document.querySelector(".pri");
+const od = document.querySelector(".atc");
+
+
+const cart = document.querySelector('.cart');
+
 // tog sec
 
     //open
     function opo() {
-        var x = document.getElementById("meno");
+        let x = document.getElementById("meno");
         if (x.style.display === "none") {
         x.style.display = "block";
         } else {
@@ -11,7 +24,7 @@
     }
     // close
     function colo() {
-        var x = document.getElementById("meno");
+        let x = document.getElementById("meno");
         if (x.style.display === "none") {
         x.style.display = "block";
         } else {
@@ -37,27 +50,108 @@
         min.src ='./images/image-product-4.jpg' ;
     }
 
+    // mb img
+
+    const ba =document.querySelector(".ba")
+    const gn =document.querySelector(".gn")
+    
+    
 // tex atc
 
-const add = document.getElementById('add');
-const nag = document.getElementById('nag');
-let nam = document.getElementById('nam');
-let nom = 0
-
 add.addEventListener('click' , function(){
+  
+  let re = nom++ +1
+  nam.textContent = re;
 
-    re = nom++
-    nam.innerText = re
+    if(nom <= 0){
+        nag.style.opacity=('0.3')
+        od.style.opacity=('0.6');
+        nom = 0 
+        nam.textContent = "0";
+    }
+    else{
+        nag.style.opacity=('1');
+        od.style.opacity=('1');
+    }
+
 });
 
 nag.addEventListener('click' , function(){
 
-    re = nom--
-    nam.innerText = re
+    let re = nom-- -1
+    nam.textContent = re;
 
+    if(nom <= 0){
+        nag.style.opacity=('0.3')
+        od.style.opacity=('0.6');
+        nom = 0 
+        nam.textContent = "0"
+    }
+    else{
+        nag.style.opacity=('1');
+        od.style.opacity=('1');
+    }
 
 });
 
-//cart
+
+// cart tog
+
+cart.addEventListener('click', () =>{
+
+        let x = document.querySelector('.cartatc');
+        if (x.style.display === "none"){
+        x.style.display = "block";
+        } else {
+        x.style.display = "none";
+        }
+    });
+
+
+// add cart
+
+od.addEventListener('click', function(){
+   
+    if(nom >= 1){
+
+        let x = document.querySelector('.zaid');
+
+            x.style.display = "block";
+            x.textContent = nom;
+            let d = document.querySelector('.ycie');
+            let s = document.querySelector('.cmlo');
+
+             let pr = document.getElementById('comtt');
+             let tot = document.querySelector('.cmla');
+
+             let res =  125 * nom;
+
+             pr.textContent = nom;
+             tot.textContent = res;
+
+            if(s.style.display === "none"){
+
+                d.style.display = "none";
+                s.style.display = "block";
+             
+            }else{  
+                s.style.display = "block";
+                d.style.display = "none";
+                }
+
+     }
+
+     
+});
+
+ function dele(){
+    let s = document.querySelector('.cmlo');
+    let d = document.querySelector('.ycie');
+    let x = document.querySelector('.zaid');
+
+    s.style.display = "none";
+    d.style.display = "block";
+    x.style.display = "none";
+ }
 
 
